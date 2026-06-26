034d2a0ae1597d46099330bac69c2824778cb214
Null <theonueche@gmail.com>
2026-06-26 23:17:42 +0100

    Create Main.kt

---
+++ app/src/main/java/com/rk/demo/Main.kt
@@ -0,0 +1,26 @@
+package com.rk.demo
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.annotation.Keep
+
+@Keep
+@Suppress("unused")
+class Main(context: ExtensionContext) : ExtensionAPI(context) {
+
+    override fun onExtensionLoaded() {}
+
+    override fun onInstalled() {}
+
+    override fun onUninstalled() {}
+
+    override fun onUpdated() {}
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {}
+
+    override fun onActivityResumed(activity: Activity) {}
+
+    override fun onActivityPaused(activity: Activity) {}
+
+    override fun onActivityDestroyed(activity: Activity) {}
+}
